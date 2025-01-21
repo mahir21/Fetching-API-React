524c77d441cd05fe80f8e775df92c5adad41faff
mahir21 <mahirjeet@gmail.com>
2025-01-21 18:27:53 -0500

    Fetching the api without using axios

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Weather from "./Weather";
+import Weather1 from "./Weather1";
+
+const App = () => {
+  return (
+    <div>
+      <Weather1></Weather1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Weather1.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect, React } from "react";
+
+const Weather1 = () => {
+  const [weather, setWeather] = useState({});
+
+  const [city, setCity] = useState("");
+
+  const [load, setLoad] = useState(false);
+
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b71be8aee5b3d4a93b5f98b6abef559c`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setWeather(data);
+      })
+      .catch((err) => {
+        console.log("Failed To Load The Data");
+      });
+  }, [city]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter Weather Value"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      ></input>
+
+      <h2>Weather in {weather?.name}</h2>
+      <h1>{weather?.main?.temp - 273}</h1>
+    </div>
+  );
+};
+
+export default Weather1;
